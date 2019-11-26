6c8cd8620608faabff47957407a2f8071de20a1b
krishna katwal <kousalkc@gmail.com>
2019-11-25 22:18:26 -0500

    finished problem 5

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -241,8 +241,19 @@ public class EvaluationService {
 		 * NANP-countries, only 1 is considered a valid country code.
 		 */
 		public String cleanPhoneNumber(String string) {
-
-			return null;
+		
+		StringBuilder arr= new StringBuilder();
+		
+		for(int i=0;i<string.length();i++) {
+			if(Character.isDigit(string.charAt(i))){
+				arr.append(string.charAt(i));	
+			}
+		}
+		if(arr.charAt(0)=='1') {
+			arr=arr.delete(0, 1);
+		}
+		String num= arr.toString();
+			return num;
 		}
 
 		/**
